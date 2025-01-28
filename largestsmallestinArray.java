e7626c61714b91d3f740cd2dd4ae39ca28f007a1
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 02:02:57 +0530

    Update largestsmallestinArray.java

--- largestsmallestinArray.java
+++ largestsmallestinArray.java
@@ -1,12 +1,14 @@
-public class largestsmallestinArray {
+import java.util.Scanner;
+
+public class LargestNo {
     public static void main(String[] args) {
-        int arr[] = {2, 4, 6, 8, 0}; 
-        int smallest = arr[0], largest = arr[0]; // Initialize smallest and largest
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > largest) largest = arr[i]; // Update largest if needed
-            else if (arr[i] < smallest) smallest = arr[i]; // Update smallest if needed
-        }
-        System.out.println("The smallest number in the array is = " + smallest); 
-        System.out.println("The largest number in the array is = " + largest); 
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt(); // Input three numbers
+        if (a > b && a > c) 
+            System.out.println("a is largest"); // a is largest
+        else if (b > a && b > c) 
+            System.out.println("b is largest"); // b is largest
+        else 
+            System.out.println("c is largest"); // c is largest
     }
 }

0f128517f2cec27a255a12cb430e6bcb88d91bfb
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 02:01:02 +0530

    Update largestsmallestinArray.java

--- largestsmallestinArray.java
+++ largestsmallestinArray.java
@@ -1,18 +1,12 @@
 public class largestsmallestinArray {
     public static void main(String[] args) {
-        int arr[] = {2,4,6,8,0};
-        int smallest = arr[0];
-        int largest = arr[0];
+        int arr[] = {2, 4, 6, 8, 0}; 
+        int smallest = arr[0], largest = arr[0]; // Initialize smallest and largest
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > largest) {
-                largest = arr[i];
-            } else if (arr[i] < smallest) { 
-                smallest = arr[i];
-            }
+            if (arr[i] > largest) largest = arr[i]; // Update largest if needed
+            else if (arr[i] < smallest) smallest = arr[i]; // Update smallest if needed
         }
-        System.out.println("The smallest number in the array is = ");
-        System.out.println(smallest);
-        System.out.println("The largest number in the array is = ");
-        System.out.println(largest);
+        System.out.println("The smallest number in the array is = " + smallest); 
+        System.out.println("The largest number in the array is = " + largest); 
     }
 }
